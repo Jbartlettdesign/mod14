be1510f9a4d3be82eab705b4e866c1575253cf6c
John <jbartlettdesign@gmail.com>
2021-06-25 14:46:24 -0500

    handlebars

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+/////allow us to use public folder/css/js
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+/////allow us to use public folder/css/js
 
+app.use(express.static(path.join(__dirname, 'public')));
+//////////////////////////////
+
+//////////////////////////////////
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+//handlebars stuff
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
